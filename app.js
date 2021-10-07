59780f83fbef9518149fe8e014db3b4b63bd1aca
Shreyansh Gupta <shreyansh.gupta@bigbinary.com>
2021-10-07 14:13:23 +0530

    Adds setup to use filesystem to write normalised data to an output file.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 // const pretty = require("pretty");
+const fs = require("fs");
 
 const ISO_URL = "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3";
 
@@ -26,6 +27,19 @@ const getISOCodes = async () => {
 
     console.dir(countries);
     // console.log(pretty(markup.html()));
+
+    fs.writeFile(
+      "countriesISO.json",
+      JSON.stringify(countries, null, 2),
+      (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          console.log("Data written to file.");
+        }
+      }
+    );
   } catch (error) {
     console.log(error);
   }
